b30ce51c9e49bc9059adbe6391cc8ba9845e952f
LafTools Team - FX <work7z@outlook.com>
2024-06-13 00:47:55 +0800

    chore: Update dependencies for @raycast/api and @types/react to latest versions

--- miaoda-api/index.ts
+++
@@ -1,9 +0,0 @@
-export type MiaodaExtension = {
-    id: string,
-    name: string;
-    version: string;
-    description: string;
-}
-export const registerMiaoda = (extInfo: MiaodaExtension) => {
-    console.log(JSON.stringify(extInfo, null, 2))
-}
