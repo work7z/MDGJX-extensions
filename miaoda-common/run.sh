3be1b03477cd8b95ddb41d9d8d73014658537a64
LafTools Team - FX <work7z@outlook.com>
2024-06-18 23:02:46 +0800

    fixed it

--- miaoda-common/run.sh
+++ miaoda-common/run.sh
@@ -4,21 +4,32 @@
 cd $(dirname $0)
 version=`jq -r .version package.json`
 echo "version: $version"
-rawMTypesDir=$PWD/m-types
-mTypesDir=$PWD/m-types/$version
+rawMTypesDir=$PWD/m-types-copy
+mTypesDir=$PWD/m-types-copy/$version
 mkdir -p $mTypesDir
 rm -rf $mTypesDir/*
 mkdir -p $mTypesDir
 cp -a *.ts $mTypesDir
+
+doWork(){
+  dir=$1
+  echo "copy $dir"
+  mkdir -p $dir
+  rm -rf $dir/*
+  cp -a $rawMTypesDir/* $rawMTypesDir/.* $dir
+}
 
 extDir=$MDGJX_EXT_ROOT/extensions
 for dir in $(ls $extDir)
 do
   if [ -d $extDir/$dir ]; then
     echo "copy $dir"
-    targetMTypesDir=$extDir/$dir/
-    rm -rf $targetMTypesDir
-    mkdir -p $targetMTypesDir
-    cp -a $rawMTypesDir $targetMTypesDir
+    targetMTypesDir=$extDir/$dir/m-types-copy/
+    doWork $targetMTypesDir
   fi
 done
+
+
+doWork $MDGJX_ROOT/modules/web-server/src/m-types-copy/
+doWork $MDGJX_ROOT/modules/web/src/m-types-copy/
+
