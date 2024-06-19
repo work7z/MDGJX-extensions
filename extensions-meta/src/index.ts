8ad5e238a960c628f1182eaada902ee562cf2c0f
LafTools Team - FX <work7z@outlook.com>
2024-06-19 23:19:28 +0800

    chore: Update Lodash extension metadata and name

--- extensions-meta/src/index.ts
+++ extensions-meta/src/index.ts
@@ -1,28 +1,34 @@
-import helloWorld from "./ext/hello-world";
-import lodash from "./ext/lodash";
+
 import _ from "lodash";
-import { MiaodaBasicConfig } from "./m-types-copy/base/m-types-main";
+import { MiaodaBasicConfig, REGISTER_CONFIG_OBJ } from "./m-types-copy/base/m-types-main";
 import path from "path";
 import fs from 'fs'
+
+// EXT LIST BEGIN
+import "./ext/hello-world";
+import "./ext/lodash";
+import "./ext/nested-codegen-web";
+// EXT LIST END
 
 const extList: {
     [key: string]: MiaodaBasicConfig;
 } = {
-    lodash,
-    helloWorld,
 };
+_.forEach(REGISTER_CONFIG_OBJ, (x, d, n) => {
+    extList[x.id] = x
+});
 
 console.log(JSON.stringify(extList));
 const MDGJX_EXT_ROOT = process.env.MDGJX_EXT_ROOT;
-const prevIdObj ={}
+const prevIdObj = {}
 _.forEach(extList, (x, d, n) => {
-    if(prevIdObj[x.id]){
-        throw new Error('id '+x.id+' is duplicated')
+    if (prevIdObj[x.id]) {
+        throw new Error('id ' + x.id + ' is duplicated')
     }
     prevIdObj[x.id] = true
-    console.log('processing '+x.id)
+    console.log('processing ' + x.id)
     const currentCwd = x.cwd || path.join(MDGJX_EXT_ROOT, "extensions", x.id);
     const finalMiaodaDistFile = path.join(currentCwd, "miaoda-dist.json");
-    console.log('writing file to '+finalMiaodaDistFile);
-    fs.writeFileSync(finalMiaodaDistFile, JSON.stringify(x, null, 2))
+    console.log('writing file to ' + finalMiaodaDistFile);
+    fs.writeFileSync(finalMiaodaDistFile, JSON.stringify(x))
 });
