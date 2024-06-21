f60290b8462b6e206a0972fc1f194c8938a79085
LafTools Team - FX <work7z@outlook.com>
2024-06-22 03:20:52 +0800

    fix

--- pipeline/upload-miaoda-ext.sh
+++ pipeline/upload-miaoda-ext.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source ~/.zshrc
 cd $(dirname $0)/..
 ROOTDIR=$PWD
 ROOTPKGDIR=$PWD/pkg
@@ -15,8 +16,10 @@ fi
 echo "releaseOrTest: $releaseOrTest"
 
 extPkgDir=/home/appuser/extstatic/ext-pkg-$releaseOrTest
+echo "extPkgDir: $extPkgDir"
 ssh $SERVER_2H2G -p 26609 "mkdir -p $extPkgDir"
 extPkgInfoDir=/home/appuser/extstatic/ext-pkginfo-$releaseOrTest
+echo "extPkgInfoDir: $extPkgInfoDir"
 ssh $SERVER_2H2G -p 26609 "mkdir -p $extPkgInfoDir"
 sftp -P 26609  $SERVER_2H2G <<< "put $ROOTPKGDIR/* $extPkgDir"
 sftp -P 26609  $SERVER_2H2G <<< "put $ROOTPKGINFODIR/* $extPkgInfoDir"
