185a190b3673a577e49468c663ee37d30b5d4887
LafTools Team - FX <work7z@outlook.com>
2024-06-22 04:22:35 +0800

    chore: Update ext-version.json to ext-v2016.07.20 and hello-world.ts to version 1.0.3

--- extensions-meta/src/ext/hello-world.ts
+++ extensions-meta/src/ext/hello-world.ts
@@ -4,7 +4,7 @@ export default fn_miaoda_registerConfig({
   mode: "miaoda",
   name: "测试模板 - Hello World",
   id: "hello-world",
-  version: "1.0.2",
+  version: "1.0.3",
   logo: "./assets/icon.png",
   shortDesc: "你好，世界！这是测试模板。",
   description: "# 这是基本的描述内容，后续可以通过@REAEDME的方式进行额外扩展",
