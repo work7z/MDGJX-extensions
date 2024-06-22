edbc21e26e7b5dee2fc2aac76a13de128d56cabb
LafTools Team - FX <work7z@outlook.com>
2024-06-22 13:14:43 +0800

    chore: Remove unnecessary files and configurations

---
+++ extensions-meta/src/ext/SRK-Toolbox.ts
@@ -0,0 +1,24 @@
+import { fn_miaoda_registerConfig } from "../m-types-copy/base/m-types-main";
+
+// Note: 目前基于最后一次成功commit进行编译，commit-id: 0cd3d77 
+
+//
+
+export default fn_miaoda_registerConfig({
+  mode: "miaoda",
+  name: "SRK转换工具集(CyberChef)",
+  id: "SRK-Toolbox",
+  version: "1.0.0",
+  logo: "./assets/icon.png",
+  shortDesc:
+    "本插件可以帮助您将文本进行各类处理，包括广泛的编码和解码操作，包括Base64、URL编码、HTML实体编码等。在此，我们要特别感谢开发者Raka-loah的汉化功能，感谢一起共建开源社区！",
+  homepage: ["https://github.com/Raka-loah/SRK-Toolbox"],
+  authors: ["Raka-loah"],
+  description: "",
+  development: {
+    entryLink: "http://localhost:40001",
+  },
+  menus: [],
+  keywords: [],
+  include: ["dist"],
+});
