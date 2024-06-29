623c58074bd3cbdb8a46959bfdda0306e502a58c
LafTools Team - FX <work7z@outlook.com>
2024-06-29 21:41:47 +0800

    Update favicon and page title for Listen to Nature extension

--- extensions/listen-to-nature/src/App.tsx
+++ extensions/listen-to-nature/src/App.tsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  type AudioType = {
+    src: string
+  }
+  const [songs, onSongs] = useState<AudioType[]>([])
 
   return (
     <>
       <h1>
-        轻听自然
+        <audio src="./sounds/bgm/chopin.mp3"  ref={e=>{
+          e?.play()
+        }} controls></audio>
       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
