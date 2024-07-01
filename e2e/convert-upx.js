0499fc8d1f63136d494596ac2ac366f06ea51abb
LafTools Team - FX <work7z@outlook.com>
2024-07-01 23:02:11 +0800

    chore: Add 'upx-example' to .gitignore

---
+++ e2e/convert-upx.js
@@ -0,0 +1,49 @@
+// node ./convert-upx.js 
+
+const fs = require("fs");
+const path = require("path");
+const compressing = require("compressing");
+const asar = require("asar");
+
+// 检查命令行参数是否提供了要处理的文件路径
+if (process.argv.length !== 3) {
+  console.error("请提供要处理的文件路径作为命令行参数！");
+  process.exit(1);
+}
+
+// 获取要处理的文件路径
+const filePath = process.argv[2];
+
+// 检查文件是否存在
+if (!fs.existsSync(filePath)) {
+  console.error(`文件 '${filePath}' 不存在！`);
+  process.exit(1);
+}
+
+// 获取文件名和后缀名
+const fileName = path.basename(filePath);
+const fileExt = path.extname(fileName);
+
+// 检查文件后缀是否为.upx
+if (fileExt !== ".upx") {
+  console.error("文件必须具有.upx扩展名！");
+  process.exit(1);
+}
+
+// 解压.upx文件
+async function upxExtract(upxPath) {
+  try {
+    const folderPath = upxPath.replace(".upx", "");
+    console.log('folderPath:', folderPath);
+    await compressing.gzip.uncompress(upxPath, folderPath + ".asar");
+    console.log("已解压.gz文件");
+
+    // 使用asar库解压.asar文件
+    await asar.extractAll(folderPath + ".asar", folderPath);
+    console.log("已解压.asar文件");
+  } catch (error) {
+    console.error("处理文件时出错:", error);
+  }
+}
+
+upxExtract(filePath);
---
+++ e2e/run-convert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+node ./convert-upx.js ./upx/AHK文档-v0.1.1.upx
