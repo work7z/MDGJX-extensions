0aa7e32012c5c1de00ab2bb7ba955206fad3f620
LafTools Team - FX <work7z@outlook.com>
2024-07-03 23:56:26 +0800

    chore: Update .gitignore to exclude 'upx-backup' file and remove unused code

--- e2e/run-convert.sh
+++ e2e/run-convert.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-node ./convert-upx.js ./upx/AHK文档-v0.1.1.upx
+set -e
+for eachItem in $(ls ./upx | grep -v upx); do
+#   node ./convert-upx.js ./upx/$eachItem
+    echo "removing $eachItem"
+    rm -rf ./upx/$eachItem
+done
+
+for eachItem in $(ls ./upx); do
+    echo "extracting $eachItem"
+    node ./convert-upx.js ./upx/$eachItem
+done
