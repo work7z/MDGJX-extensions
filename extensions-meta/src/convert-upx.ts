9338b5413d9a2b0a961e2f09503db340a7e0cd50
LafTools Team - FX <work7z@outlook.com>
2024-07-04 23:50:34 +0800

    chore: Update convert-upx.ts to include menu items in miaoda-dist.json

--- extensions-meta/src/convert-upx.ts
+++ extensions-meta/src/convert-upx.ts
@@ -1,11 +1,17 @@
-// node ./convert-upx.js 
+// node ./convert-upx.js
 
-import { ExternalConfigA, MiaodaBasicConfig } from "./m-types-copy/base/m-types-main";
+import _ from "lodash";
+import {
+  ExternalConfigA,
+  MiaodaBasicConfig,
+  SystemSubModuleItem,
+} from "./m-types-copy/base/m-types-main";
 
 const fs = require("fs");
 const path = require("path");
 const compressing = require("compressing");
 const asar = require("asar");
+const TinyPinyin = require('tiny-pinyin')
 
 // 检查命令行参数是否提供了要处理的文件路径
 if (process.argv.length !== 3) {
@@ -36,7 +42,7 @@ if (fileExt !== ".upx") {
 async function upxExtract(upxPath) {
   try {
     const folderPath = upxPath.replace(".upx", "");
-    console.log('folderPath:', folderPath);
+    console.log("folderPath:", folderPath);
     await compressing.gzip.uncompress(upxPath, folderPath + ".asar");
     console.log("已解压.gz文件");
 
@@ -45,37 +51,57 @@ async function upxExtract(upxPath) {
     console.log("已解压.asar文件");
 
     const pluginJson = path.join(folderPath, "plugin.json");
-    if(!fs.existsSync(pluginJson)) {
+    if (!fs.existsSync(pluginJson)) {
       console.error("未找到plugin.json文件！");
       process.exit(1);
-    }else{
+    } else {
       console.log("找到plugin.json文件！");
     }
+    
     // jsdoc
-    const thePlugin:ExternalConfigA = JSON.parse(fs.readFileSync(pluginJson, "utf-8"));
-    const newConfig:MiaodaBasicConfig = {
+    const thePlugin: ExternalConfigA = JSON.parse(
+      fs.readFileSync(pluginJson, "utf-8")
+    );
+    const eachToolNavInfo = thePlugin;
+    const enName = _.toLower(
+      TinyPinyin.convertToPinyin(eachToolNavInfo.pluginName) +
+        "-" +
+        eachToolNavInfo.version
+    );
+    const newConfig: MiaodaBasicConfig = {
       id: thePlugin.name,
       version: thePlugin.version,
       name: thePlugin.pluginName,
       shortDesc: thePlugin.description,
       description: "",
       development: {
-        entryLink: ""
+        entryLink: "",
       },
       runtime: {
         type: "external-config-a",
-        external_CFG_A: thePlugin
+        external_CFG_A: thePlugin,
       },
       include: [],
       menus: [
-        // TODO: show the menus
-        // {
-        //   id: thePlugin.pluginName,
-        //   name: thePlugin.pluginName,
-        //   icon: "extension",          
-        // }
-      ]
-    }
+        {
+          id: "p-" + enName,
+          iconInStr: "Briefcase", // 应该用真实的icon来显示
+          belongTo: "tools",
+          name: eachToolNavInfo.name,
+          children: [
+            {
+              id: enName,
+              iconInStr: "AppWindows",
+              disableFooter: true,
+              name: eachToolNavInfo.name,
+              moduleItemtURL: `/ext-view/${enName}`,
+              keywords: [],
+              description: eachToolNavInfo.description,
+            } satisfies SystemSubModuleItem,
+          ],
+        } satisfies SystemSubModuleItem,
+      ],
+    };
     const miaodaDistFile = path.join(folderPath, "miaoda-dist.json");
     fs.writeFileSync(miaodaDistFile, JSON.stringify(newConfig, null, 2));
     console.log("已生成miaoda-dist.json文件");
--- pipeline/run-convert-upx.sh
+++ pipeline/run-convert-upx.sh
@@ -5,20 +5,17 @@ npm run compile
 
 cd $MDGJX_EXT_ROOT/e2e
 set -e
-for eachItem in $(ls ./upx | grep -v upx); do
+cd $MDGJX_EXT_ROOT/e2e
+[ -d ./upx-temp ] && rm -rf ./upx-temp
+mkdir -p ./upx-temp
+cp -a ./upx/* ./upx-temp
+for eachItem in $(ls ./upx-temp | grep -v upx); do
 #   node ./convert-upx.js ./upx/$eachItem
     echo "removing $eachItem"
-    rm -rf ./upx/$eachItem
+    rm -rf ./upx-temp/$eachItem
 done
 
-for eachItem in $(ls ./upx); do
+for eachItem in $(ls ./upx-temp); do
     echo "extracting $eachItem"
-    node $MDGJX_EXT_ROOT/extensions-meta/dist/convert-upx.js $PWD/upx/$eachItem
+    node $MDGJX_EXT_ROOT/extensions-meta/dist/convert-upx.js $PWD/upx-temp/$eachItem
 done
-[ -d ./upx-temp ] && rm -rf ./upx-temp
-mkdir ./upx-temp
-for eachItem in $(ls ./upx | grep -v upx); do
-    echo "moving $eachItem"
-    mv ./upx/$eachItem ./upx-temp/$eachItem
-done
-rm ./upx-temp/*.asar
